b1d2e69d2f1150fe01738d98c199b58f27c1e227
Warren_Lin <warren_lin@cmoney.com.tw>
2021-05-11 10:30:09 +0800

    feat: fix fragment demo

--- FragmentDemo/app/src/main/java/com/cmoney/fragmentdemo/FirstFragment.kt
+++ FragmentDemo/app/src/main/java/com/cmoney/fragmentdemo/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.cmoney.fragmentdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,18 +12,23 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("Test", "onCreateView")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.i("Test", "onViewCreated")
 
         view.findViewById<Button>(R.id.button).apply {
             setOnClickListener {
                 val fragmentTransaction = requireFragmentManager().beginTransaction()
 //                fragmentTransaction.replace(R.id.frameLayout, SecondFragment())
-                fragmentTransaction.add(R.id.frameLayout, SecondFragment()).hide(this@FirstFragment)
+//                fragmentTransaction.add(R.id.frameLayout, SecondFragment()).hide(this@FirstFragment)
+                fragmentTransaction.add(R.id.frameLayout, SecondFragment.newInstance("test1", "test2")).hide(this@FirstFragment)
+
+
                 fragmentTransaction.addToBackStack(null)
                 fragmentTransaction.commit()
             }
--- FragmentDemo/app/src/main/java/com/cmoney/fragmentdemo/SecondFragment.kt
+++ FragmentDemo/app/src/main/java/com/cmoney/fragmentdemo/SecondFragment.kt
@@ -1,6 +1,7 @@
 package com.cmoney.fragmentdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -38,6 +39,9 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.i("SecondFragment", "param1 = $param1")
+        Log.i("SecondFragment", "param2 = $param2")
+
         val button = view.findViewById<Button>(R.id.button)
         button.setOnClickListener {
             val fragmentTransaction = fragmentManager?.beginTransaction()
